d9a5c95c0ce4222202f73b2fc9c2772c609c1695
peoft <Paulo Filho>
2017-07-06 23:03:35 -0300

    Organização da estrutura.
    Adicionado o arquivo AV1.sql ao diretório sql e feitas ataulizações no
    mesmo quanto a chave primária das tabelas funcionário e motorista.

--- sql/AV1.sql
+++ sql/AV1.sql
@@ -15,23 +15,25 @@ numeroCNH varchar(255) NOT NULL,
 FOREIGN KEY ( pessoa_id )
 	REFERENCES Pessoa(id)
     ON DELETE CASCADE
-    ON UPDATE CASCADE);
+    ON UPDATE CASCADE,
+PRIMARY KEY (numeroCNH));
 
 CREATE TABLE Funcionario (
 pessoa_id INT NOT NULL,
-matricula varchar(255),
+matricula varchar(255) NOT NULL,
 FOREIGN KEY ( pessoa_id )
 	REFERENCES Pessoa(id)
     ON DELETE CASCADE
-    ON UPDATE CASCADE);
+    ON UPDATE CASCADE,
+PRIMARY KEY (matricula));
 
 CREATE TABLE Aluguel (
 id INT NOT NULL AUTO_INCREMENT,
 pessoa_id INT NOT NULL,
-dataPedido date,
-dataEntrega date,
+dataPedido date NOT NULL,
+dataEntrega date NOT NULL,
 dataDevolucao date,
-valorTotal double,
+valorTotal double NOT NULL,
 FOREIGN KEY ( pessoa_id )
 	REFERENCES Pessoa(id),
 PRIMARY KEY ( id ) );
